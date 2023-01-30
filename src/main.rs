1d500d6ca772b7bcd98a6eb52985abf42858c10a
Marcos Flavio <mfghost69@gmail.com>
2023-01-29 22:14:12 -0300

    Ampliando o programa e colocando mais funcionallidades

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,51 @@ use rand::Rng;
 
 fn main() {
     greetings();
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    let lenght: u8 = input.trim().parse().expect("Please type a number!");
-    let password = generate_password(lenght);
-    println!("Your password is: {}", password);
+    loop { 
+        println!("Press:\n1 - Generate new password.\n2 - See saved passwords");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let response: u8 = input.trim().parse().expect("Please type 1 or 2");
+        if response == 1 {
+            loop {
+                let _password = new_password();
+                println!("1 - Generate new password\n2 - Save password\n3 - Finish program");
+                let mut response = String::new();
+                io::stdin().read_line(&mut response).expect("Failed to read line");
+                let response: u8 = response.trim().parse().expect("Please type 1, 2 or 3");
+                match response {
+                    1 => continue,
+                    2 => panic!("Sorry, not yet implemented"),
+                    3 => std::process::exit(0),
+                    _ => panic!("Please type one of the options(1, 2 or 3)"),
+                }
+            }
+        }
+        else if response == 2 {
+            panic!("Not yet implemented");
+        }
+        else {
+            panic!("Please typ 1 ot 2");
+        }
+    }
 }
 
 fn greetings(){
-    println!("Hello, this is your password generator.\nPlease infor the lenght: ");
+    println!("Hello, this is your password app.\n");
+}
+
+fn new_password() -> String {
+    let mut input = String::new();
+    println!("Please type the lenght of the password:");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let lenght: u8 = input.trim().parse().expect("Please type a number");
+    let password = generate_password(lenght);
+    println!("your new password is: {}", password);
+    password
 }
 
 fn generate_password(lenght: u8) -> String {
