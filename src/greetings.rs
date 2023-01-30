d217119353bb050adef6bd867c72822bbd5f4f08
Marcos Flavio <mfghost69@gmail.com>
2023-01-30 03:04:52 -0300

    Fixing message

--- src/greetings.rs
+++ src/greetings.rs
@@ -1,4 +1,4 @@
 pub fn greeting() {
     println!("Welcome, this is you password manager\n****************************************************");
-    println!("Here you are able to generate and manage diferent and complex passwords for your security\n");
+    println!("Here you are able to generate and manage different and complex passwords for your security\n");
 }
