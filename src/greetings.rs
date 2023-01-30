0e51eacadc6c19f220fc5f90e02760ccc38576b6
Marcos Flavio <mfghost69@gmail.com>
2023-01-30 02:59:46 -0300

    Subdividing functions ins diferent files(modules)

---
+++ src/greetings.rs
@@ -0,0 +1,4 @@
+pub fn greeting() {
+    println!("Welcome, this is you password manager\n****************************************************");
+    println!("Here you are able to generate and manage diferent and complex passwords for your security\n");
+}
---
+++ src/new_pass.rs
@@ -0,0 +1,41 @@
+use rand::Rng;
+use std::io;
+
+pub fn new_password() -> String {
+    let mut input = String::new();
+    println!("Please type the lenght of the password:");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let lenght: u8 = input.trim().parse().expect("Please type a number");
+    let password = generate_password(lenght);
+    println!("your new password is: {}", password);
+    password
+}
+
+pub fn generate_password(lenght: u8) -> String {
+    let mut password: String = String::new();
+    for _ in 0..lenght {
+        password.push(get_random_char());
+    }
+    password
+}
+
+pub fn get_random_char() -> char {
+    let mut rng = rand::thread_rng();
+    let random_number: u8 = rng.gen_range(0..=3);
+    match random_number {
+        0 => rng.gen_range('a'..='z'),
+        1 => rng.gen_range('A'..='Z'),
+        2 => rng.gen_range('0'..='9'),
+        3 => special_chr(&mut rng),
+        _ => panic!("Something went wrong!"),
+    }
+}
+
+pub fn special_chr(rng: &mut rand::rngs::ThreadRng) -> char {
+    let special_chars = "*+-&%$@!><?~";
+    let index = rng.gen_range(0..special_chars.len());
+    let random_char = special_chars.chars().nth(index).unwrap();
+    random_char
+}
