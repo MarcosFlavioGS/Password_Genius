c3f3c1bcc057a96dfeec930ef8bc8728e717bb30
Marcos Flavio <mfghost69@gmail.com>
2023-02-28 21:47:46 -0300

    Removal: Removing to_owened() from clipboarder because the password passed already is a String type.

--- src/clipboarding.rs
+++ src/clipboarding.rs
@@ -2,6 +2,6 @@ use clipboard::{ClipboardContext,ClipboardProvider};
 
 pub fn clipboarder(password: String) -> Result<(), Box<dyn std::error::Error>> {
     let mut ctx: ClipboardContext = ClipboardProvider::new()?;
-    ctx.set_contents(password.to_owned())?;
+    ctx.set_contents(password)?;
     Ok(())
 }
