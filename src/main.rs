f6b1a4ab2589ebcb3b5957a00c1033c68deeb14a
Marcos Flavio <mfghost69@gmail.com>
2023-03-13 00:24:32 -0300

    Refactoring: Substituting ifs an elses with match patterns

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-mod new_pass;
-mod greetings;
 mod clipboarding;
+mod greetings;
+mod new_pass;
 
-use std::io;
-use new_pass::new_password;
-use greetings::greeting;
 use clipboarding::clipboarder;
+use greetings::greeting;
+use new_pass::new_password;
+use std::io;
 
 fn main() {
     greeting();
     // TODO: Implement autenticating system with database
-    loop { 
+    loop {
         println!("Press:\n1 - Generate new password.\n2 - See saved passwords\n3 - Finish Program");
         let mut input = String::new();
         io::stdin()
@@ -18,8 +18,8 @@ fn main() {
             .expect("Failed to read line");
         let response: u8 = input.trim().parse().expect("Please type 1, 2 or 3");
         // Generating new password
-        if response == 1 {
-            loop {
+        match response {
+            1 => loop {
                 let _password = new_password(); // Pass generated
                 match clipboarder(_password) {
                     Ok(_) => println!("Password copied to clipboard !"),
@@ -37,17 +37,10 @@ fn main() {
                     3 => std::process::exit(0),
                     _ => println!("\n{} is not valid, please type one of the options.\nGenerating new password.", response),
                 }
-            }
-        }
-        else if response == 2 {
-            panic!("Not yet implemented");// TODO: Redirect to interaction with database
-        }
-        else if response == 3 {
-            std::process::exit(0);
-        }
-        else {
-            println!("Please type one of the options available");
+            },
+            2 => panic!("Not yet implemented"), // TODO: Redirect to interaction with database
+            3 => std::process::exit(0),
+            _ => println!("Please type one of the options available"),
         }
     }
 }
-
