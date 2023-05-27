3cff037793059f3145978d62f6f4939f29074bad
Marcos Flavio <mfghost69@gmail.com>
2023-05-27 20:15:09 -0300

    Feat (generate): Add clipboarder back to generate.

--- src/generate.rs
+++ src/generate.rs
@@ -1,10 +1,17 @@
 use crate::insert_pass::insert_pass;
 use crate::new_pass::new_password;
+use crate::clipboarding::clipboarder;
 
 pub fn generate(path: &str) {
     let passwd = new_password();
     match insert_pass(path, &passwd) {
         Ok(_) => println!("Inserted at: {path}"),
         Err(err) => println!("Failed to insert password.\nError: {err}"),
+    }
+    match clipboarder(&passwd[..]) {
+        Ok(_) => println!("Copied to clipboard"),
+        Err(err) => println!(
+            "Failed to read line.\nError: {err}"
+        ),
     }
 }
