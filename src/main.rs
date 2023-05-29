0b1c8a8b92ec9b147ac038ab47a8c24a28df4264
Marcos Flavio <mfghost69@gmail.com>
2023-05-29 20:35:19 -0300

    Refact (main.rs)Ã: Removing lines

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,14 @@ fn main() {
         1 => println!("TODO: Show filetree of all created passwords."),
         (2..=3) => match &args[1][..] {
             "generate" => {
-                let filepath = &args[2][..];
                 let mut base_path = String::from("passgen/");
-                base_path.push_str(filepath);
+                base_path.push_str(&args[2][..]);
                 base_path.push_str("/pass");
                 generate(&base_path);
             }
             "insert" => {
-                let filepath = &args[2][..];
                 let mut base_path = String::from("passgen/");
-                base_path.push_str(filepath);
+                base_path.push_str(&args[2][..]);
                 base_path.push_str("/pass");
                 insert(&base_path);
             }
