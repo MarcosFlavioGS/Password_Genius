29ea6cf3a4334a55574e411b415b606ca902c042
Marcos Flavio <mfghost69@gmail.com>
2025-01-07 22:50:35 -0300

    fix: change deps

--- src/clipboarding.rs
+++ src/clipboarding.rs
@@ -1,7 +1,7 @@
-use cli_clipboard::{ClipboardContext, ClipboardProvider};
+use clipboard::{ClipboardContext, ClipboardProvider};
 
 pub fn clipboarder(password: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let mut ctx = ClipboardContext::new().unwrap();
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
     ctx.set_contents(password.to_owned()).unwrap();
     assert_eq!(ctx.get_contents().unwrap(), password);
     Ok(())
