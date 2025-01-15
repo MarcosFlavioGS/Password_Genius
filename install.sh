db1a2e4b16a32401b3cb62423886c102c4f1515d
Marcos Flavio <mfghost69@gmail.com>
2025-01-14 23:58:12 -0300

    chore: add an installation .sh file.

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+set -e
+
+# Compile the project
+cargo build --release
+
+# Get the target binary
+BINARY_PATH="target/release/passgen"
+
+# Define the installation path
+INSTALL_PATH="/usr/local/bin/passgen"
+
+# Copy the binary
+echo "Installing to $INSTALL_PATH"
+sudo cp "$BINARY_PATH" "$INSTALL_PATH"
+sudo chmod +x "$INSTALL_PATH"
+
+echo "Installation complete. Run 'passgen' to start."
